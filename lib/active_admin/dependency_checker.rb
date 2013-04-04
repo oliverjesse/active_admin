ab827cd71a4f1578088940741502fe738ecb5ec6
Jesse Sanford <jesse@berkeley.edu>
2013-04-04 12:15:03 -0700

    patch from Daxter

--- lib/active_admin/dependency_checker.rb
+++ lib/active_admin/dependency_checker.rb
@@ -4,6 +4,8 @@ module ActiveAdmin
   module DependencyChecker
     class << self
       def check!
+        confirm_asset_gems_are_installed unless Rails.env.production?
+
         if rails_3_1?
           unless meta_search_1_1? && sass_rails_3_1?
             warn "ActiveAdmin requires meta_search >= 1.1.0.pre and sass-rails ~> 3.1.0.rc to work with rails >= 3.1.0"
@@ -12,6 +14,17 @@ module ActiveAdmin
 
         if pry_rails_before_0_1_6?
           warn "ActiveAdmin is not compatible with pry-rails < 0.1.6. Please upgrade pry-rails."
+        end
+      end
+
+      def confirm_asset_gems_are_installed
+        %w[bourbon coffee-rails jquery-rails sass-rails].each do |name|
+          begin
+            require name
+          rescue LoadError => e
+            raise unless e.message =~ /#{name}/ # re-raise if the exception isn't directly from this gem
+            raise "ActiveAdmin depends on the #{name} gem for JS/CSS."
+          end
         end
       end
 
